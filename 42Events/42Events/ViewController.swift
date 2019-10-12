248daeb5458d08bfcfcc390ff992ca586ec76ed8
Samantha HILLEBRAND <shillebr@c1r2s3.wethinkcode.co.za>
2019-10-12 11:29:42 +0200

    Setting up

---
+++ 42Events/42Events/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  42Events
+//
+//  Created by Samantha HILLEBRAND on 2019/10/12.
+//  Copyright © 2019 Rush00Team. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
