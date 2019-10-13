5f660acc03225510882d7921ca43a184c7d7cccf
Teo <teokelestura.tk@gmail.com>
2019-10-13 21:18:39 +0200

    13/10/19
    + Set username removed

--- 42Events/42Events/LoginViewController.swift
+++ 42Events/42Events/LoginViewController.swift
@@ -77,8 +77,8 @@ class LoginViewController: UIViewController {
 //    View Did Load
     override func viewDidLoad() {
         super.viewDidLoad()
-        usernameTextField.text = "tkelest"
-        passwdTextField.text = "asd"
+        usernameTextField.text = ""
+        passwdTextField.text = ""
 //        if UIDevice.current.orientation.isLandscape {}
     }
 
